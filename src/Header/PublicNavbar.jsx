df53b815a64f958087f2e9fd6d3a5e1ccd730033
vibhuti prajapati <vibhutiprajapati784@gmail.com>
2026-01-30 19:28:21 +0530

    logo ducky  and nav changes

--- src/Header/PublicNavbar.jsx
+++ src/Header/PublicNavbar.jsx
@@ -1,22 +1,34 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import logoIcon from '../assets/duckicon.png';
 const PublicNavbar = () => {
+    const [scrolled, setScrolled] = useState(false)
+
+  useEffect(() => {
+    const onScroll = () => {
+      setScrolled(window.scrollY > 80)
+    }
+
+    window.addEventListener('scroll', onScroll)
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [])
+
   return (
     <div className='navbar bg-transparent sticky  top-0 left-0 w-full z-50'>
       <div className='navbar-start'>
         <a
-          className='btn btn-ghost text-neutral-50 font-brigends text-2xl'
+          className='btn btn-ghost  hover:text-black text-neutral-50 font-brigends text-2xl '
           href='/'
         >
+          <img src={logoIcon} alt='Dev Orbit logo icon' className='w-7 h-7' />
           DEV ORBIT
         </a>
       </div>
       <div className='navbar-center hidden lg:flex'></div>
       <div className='navbar-end'>
         <a href='/login'>
-        <button className=' btn btn-sm font-bold rounded-xl px-4 py-1.5' >
-          Log in
-        </button>
+          <button className=' btn btn-sm font-bold rounded-xl px-4 py-1.5'>
+            Log in
+          </button>
         </a>
       </div>
     </div>
