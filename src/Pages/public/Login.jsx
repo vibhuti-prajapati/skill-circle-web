f2677af708344a1a8d009becc0b805b2d46e960d
vibhuti prajapati <vibhutiprajapati784@gmail.com>
2026-01-30 18:39:26 +0530

    login page fix

--- src/Pages/public/Login.jsx
+++ src/Pages/public/Login.jsx
@@ -2,37 +2,52 @@ import React from 'react'
 
 export default function Login () {
   return (
-    <div className=' min-h-screen grid place-items-center'>
-      <form className='fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4'>
-        <fieldset className='fieldset'>
-          <label className='label'>Email</label>
-          <input
-            type='email'
-            className='input validator'
-            placeholder='Email'
-            required
-          />
-          <p className='validator-hint hidden'>Required</p>
-        </fieldset>
+    <div className='h-[calc(100vh-4rem)] grid place-items-center overflow-hidden'>
+      <div className='grid place-items-center'>
+        <form className='fieldset bg-[#393a41]  rounded-box w-80  p-4'>
+          <h1 className='text-2xl font-bold text-neutral-50 text-center mb-1'>
+            Welcome back!
+          </h1>
 
-        <label className='fieldset'>
-          <span className='label'>Password</span>
-          <input
-            type='password'
-            className='input validator'
-            placeholder='Password'
-            required
-          />
-          <span className='validator-hint hidden'>Required</span>
-        </label>
+          <fieldset className='fieldset'>
+            <label className='label text-neutral-50'>Email</label>
+            <input
+              type='email'
+              className='input validator bg-[#35353c] text-neutral-50 w-70 h-8'
+              required
+            />
+          </fieldset>
 
-        <button className='btn btn-neutral mt-4' type='submit'>
-          Login
-        </button>
-        <button className='btn btn-ghost mt-1' type='reset'>
-          Reset
-        </button>
-      </form>
+          <label className='fieldset text-neutral-50'>
+            <span className='label'>Password</span>
+            <input
+              type='password'
+              className='input validator bg-[#35353c] text-neutral-50 w-70 h-8'
+              required
+            />
+          </label>
+          <div className='text-left mt-1'>
+            <a
+              href='/forgot-password'
+              className='text-xs text-[#808afa] hover:underline'
+            >
+              Forgot your password?
+            </a>
+          </div>
+          <button
+            className='btn btn-neutral border-[#5764f0] rounded-xl mt-4 bg-[#5764f0] text-neutral-50'
+            type='submit'
+          >
+            Log in
+          </button>
+          <p className='text-xs text-neutral-400 text-center mt-4'>
+          Need an account?{' '}
+          <a href='/register' className='text-[#808afa] hover:underline'>
+            Register
+          </a>
+        </p>
+        </form>
+      </div>
     </div>
   )
 }
